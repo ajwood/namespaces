07745363990c4d08b9e45ec6777210fa63c19dd6
andrew <andrew@biospective.com>
2018-05-29 19:28:53 -0400

    initial commit

---
+++ file_a.py
@@ -0,0 +1,66 @@
+
+print("file_a.py is getting executed")
+
+# Note this gets set when the file is loaded
+global_A = 'global_A (in file_a)'
+
+class A(object):
+
+    def __init__(self, attr):
+
+        print "instantiating an A object"
+
+        # This attr is passed to the constructor
+        self.attr = attr
+
+        # These don't exist locally, so better exist in the global namespace
+        self.myglobal_A = global_A
+        self.myglobal_B = global_B
+
+    def __str__(self):
+        """A pretty printer"""
+        return "A(attr={}, global_A={}, global_B={})".format(
+                self.attr, 
+                self.myglobal_A,
+                self.myglobal_B)
+
+    def say_something(self):
+        print "Hi! I'm {}".format(self)
+
+
+class B(object):
+
+    def __init__(self, attr):
+
+        print "instantiating a B object"
+
+        # This attr is passed to the constructor
+        self.attr = attr
+
+        # These don't exist locally, so better exist in the global namespace
+        self.myglobal_A = global_A
+        self.myglobal_B = global_B
+
+    def __str__(self):
+        """A pretty printer"""
+        return "B(attr={}, global_A={}, global_B={})".format(
+                self.attr, 
+                self.myglobal_A,
+                self.myglobal_B)
+
+    def say_something(self):
+        print "Hi! I'm {}".format(self)
+
+
+if __name__ == '__main__':
+
+    print "entering the __main__ block (file_a)"
+
+    # Note this gets set when the __main__ block runs
+    global_B = 'global_B'
+
+    a = A('123 from file_a main')
+    b = B('xzy from file_a main')
+
+    a.say_something()
+    b.say_something()
---
+++ file_b.py
@@ -0,0 +1,40 @@
+from file_a import A
+from file_a import B
+
+print("file_b.py is getting executed")
+
+# Note this gets set when the file is loaded
+global_A = 'global_A (in file_b)'
+
+class C(object):
+    
+    def __init__(self, attr):
+
+        print "instantiating a C object"
+
+        # This attr is passed to the constructor
+        self.attr = attr
+
+        # These don't exist locally, so better exist in the global namespace
+        self.myglobal_A = global_A
+        self.myglobal_B = global_B
+
+    def g(self):
+        print 'calling g in a C object'
+
+
+
+
+
+if __name__ == '__main__':
+
+    print "entering the __main__ block (file_b)"
+
+    # Note this gets set when the __main__ block runs
+    global_B = 'global_B (in file_b)'
+
+    a = A('123 from file_a main')
+    b = B('xzy from file_a main')
+
+    a.say_something()
+    b.say_something()
